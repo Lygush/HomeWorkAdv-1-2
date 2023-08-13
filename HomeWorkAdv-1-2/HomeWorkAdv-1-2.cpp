a25ed10e091eb6339cc89e4961800fb6ac9404b7
Ivan <kosarevsky2000@yandex.ru>
2023-08-13 10:53:17 +0300

    initial commit

---
+++ HomeWorkAdv-1-2/HomeWorkAdv-1-2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <variant>
+
+
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+int main()
+{
+	auto result{ get_variant() };
+	if (std::holds_alternative<int>(result)) {
+		std::cout << std::get<int>(result) * 2;
+	}
+	else if (std::holds_alternative<std::string>(result)) {
+		std::cout << std::get<std::string>(result);
+	}
+	else if (std::holds_alternative<std::vector<int>>(result)) {
+		for (const int& i : std::get<std::vector<int>>(result)) {
+			std::cout << i << " ";
+		}
+	}
+}
